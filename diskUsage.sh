87c9279b67ef7e8618440ebd2445b542bf9a4ab7
AbhishekWadhwa <abhishek.wadhwa2010@gmail.com>
2017-11-29 02:24:39 +0530

    Add files via upload

---
+++ diskUsage.sh
@@ -0,0 +1,7 @@
+du -h > output.txt
+gedit output.txt
+echo "Size DirectoryPath" >> output1.txt
+cat output.txt >> output1.txt
+gedit output1.txt
+paste output1.txt > output1.xls
+libreoffice output1.xls
